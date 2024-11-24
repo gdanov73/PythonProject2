8c74d917755511660c2ae83ac47022028cb6846a
gdanov73 <gdanov73@gmail.com>
2024-11-24 07:46:50 +0300

    First commit

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Hello, world')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/#
+print('1st  program')# арифметика
+print(9**0.5*5)
+print('2nd program') # логика
+print(9.99 > 9.98 and 1000!=1000.1)
+print('3rd program') # школьная загадка
+print ('2*2+2')
+print ('2*(2+2)')
+print(2*2+2 == 2*(2+2))
+print('4th program') # первый после точки
+print('123.456')
+print(int(float(123.456*10)%10))
