b925a791d842a93e87983ab49afb77a89aa47b2e
gdanov73 <gdanov73@gmail.com>
2024-11-26 08:04:09 +0300

    Task2

--- module_1_3.py
+++ module_1_3.py
@@ -7,3 +7,8 @@ print('New age:',age)
 is_student=True
 print('is student:',is_student)
 
+homework=12 #количество выполненных ДЗ
+hour=1.5  #количество затраченных часов
+course_name='Python' #название курса
+math=hour/homework #время на одно задание
+print('Курс:',course_name,',всего задач:',homework,',затрачено часов:',hour,',среднее время выполнения',math,"часа.") #вывод на экран
