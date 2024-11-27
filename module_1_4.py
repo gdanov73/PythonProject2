78bc61750e5ad4e5e1c101724586354b9df0f0d4
gdanov73 <gdanov73@gmail.com>
2024-11-27 18:57:41 +0300

    Task2

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string=input("Введите строку: ") # создание переменной
+print(len(my_string)) # подсчет кол-ва символов
+print(my_string.upper()) # верхний регистр
+print(my_string.lower()) # нижний регистр
+print(my_string.replace(" ","")) # убрали пропуски между словами
+print(my_string[0]) # печать первого символа
+print(my_string[-1]) # печать второго символа
