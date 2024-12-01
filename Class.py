fc96454ae60917552589b062df7c5df734f449ea
gdanov73 <gdanov73@gmail.com>
2024-12-01 12:09:46 +0300

    Task2

---
+++ Class.py
@@ -0,0 +1,12 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]] # список оценок
+students = {'Johnny', 'Bilbo', 'Steve', 'Hendrik', 'Aaron'} # список студентов
+students = list(students) # замена мн на список
+students.sort() # выстраивание имен по алфавиту
+print(students) # вывод имен на экран
+journal = {} # оздание словаря
+journal[students[0]]=sum(grades[0])/len(grades[0]) # вычисление  среднего балла
+journal[students[1]]=sum(grades[1])/len(grades[1]) # -/-/-
+journal[students[2]]=sum(grades[2])/len(grades[2]) # -/-/-
+journal[students[3]]=sum(grades[3])/len(grades[3]) # -/-/-
+journal[students[4]]=sum(grades[4])/len(grades[4]) # -/-/-
+print(journal) # вывод среднего балла каждого студента на экран
--- module_1_6.py
+++ module_1_6.py
@@ -1,18 +1,18 @@
-my_dict={"Aleks":1972,"Sergei":1973,"Val":1949,"Leonid":1958,"Nina":1926,"Ivan":1924} # 1,2,3,4,5,6
-print("Dict:",my_dict)
-print("Г.р. ",my_dict["Nina"])
-print("Нет в списке",my_dict.get("Valera"))
+my_dict={"Aleks":1972,"Sergei":1973,"Val":1949,"Leonid":1958,"Nina":1926,"Ivan":1924} # создание ловаря
+print("Dict:",my_dict) # вывод на экран списка знакомых с годом рожд.
+print("Г.р. ",my_dict["Nina"]) # вывод года рожд.(значения) по имени(ключу)
+print("Нет в списке",my_dict.get("Valera")) # задаем несуществующий ключ
 
-my_dict.update({"Anna":1997,"Sandra":2000})
-print(my_dict.pop("Ivan"))
+my_dict.update({"Anna":1997,"Sandra":2000}) # добавляем еще данные 2-х человек
+print(my_dict.pop("Ivan")) # удаляем одно имя
 
-print("Обновленный список:",my_dict)
+print("Обновленный список:",my_dict) # печатаем обновленный список
 
 
 
-my_set={1,'ball',10,1917,'cub',3.14,1,'ball',10}
-print(my_set)
-my_set.remove("ball")
-print(my_set)
+my_set={1,'ball',10,1917,'cub',3.14,1,'ball',10} # создаем множ-во
+print(my_set) # выводим множ-во на экран
 my_set.add('string')
+my_set.add('cosmos') # добавляем 2 элемента
+my_set.remove("ball") # удаляем 1 элемент
 print(my_set)
