4a4c341d7288cc2b614a03b9b7272eeb28d12fdd
gdanov73 <gdanov73@gmail.com>
2024-12-03 16:13:54 +0300

    Task2

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+print("Введите 3 числа:") # начало программы
+first=int(input()) # ввод 1-го числа
+second=int(input()) # ввод 2-го числа
+third=int(input()) # ввод 3-го числа
+if first==second and second==third: # проверка условия, когда все числа равны между собой
+ print("3") #  вывод на экран результата
+elif first==second or first==third or second==third or first==third: # рпроверка равенства какой-либо пары между собой
+    print("2") # вывод результата на экран
+else:print("0") # вывод на экран результата, когда все числа разные
