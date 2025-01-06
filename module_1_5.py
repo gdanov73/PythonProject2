57919cb47dde0c7cc9705ad9f5355aeb2f34f93d
gdanov73 <gdanov73@gmail.com>
2025-01-06 08:49:25 +0300

    module 1.5

--- module_1_5.py
+++ module_1_5.py
@@ -10,3 +10,5 @@ mutable_list[1]="2" # замена 2-го элемента на цифру 2
 mutable_list[2]="3" # замена 3-го элемента на цифру 3
 print("Mutable list:")
 print(mutable_list ,"Modified") # вывод на экран обновленного списка
+
+
