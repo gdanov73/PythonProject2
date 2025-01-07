a8f5f27ee964f98ab539fb8efff1fea337811672
gdanov73 <gdanov73@gmail.com>
2025-01-07 14:53:49 +0300

    module 5.4

--- module_5_4.py
+++ module_5_4.py
@@ -1,76 +1,75 @@
-class House:
+class House: #
     def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
+        self.name = name #
+        self.number_of_floors = number_of_floors #
 
-    def go_to(self, new_floor):
-        if 1 < new_floor < self.number_of_floors:
-            for i in range(1, new_floor + 1):
-                print(i)
+    def go_to(self, new_floor): #
+        if 1 < new_floor < self.number_of_floors: #
+            for i in range(1, new_floor + 1): #
+                print(i) #
             else:
-                print('Такого этажа не существует')
+                print('Такого этажа не существует') #
 
-    def __len__(self):
+    def __len__(self): #
         return self.number_of_floors
 
-    def __str__(self):
+    def __str__(self): #
         return f"Название: {self.name}, количество этажей: {self.number_of_floors}"
 
-    def __eq__(self, other):
+    def __eq__(self, other): #
         return self.number_of_floors == other.number_of_floors
 
-    def __lt__(self, other):
+    def __lt__(self, other): #
         return self.number_of_floors < other.number_of_floors
 
-    def __le__(self, other):
+    def __le__(self, other): #
         return self.number_of_floors <= other.number_of_floors
 
-    def __gt__(self, other):
+    def __gt__(self, other): #
         return self.number_of_floors > other.number_of_floors
 
-    def __ge__(self, other):
+    def __ge__(self, other): #
         return self.number_of_floors >= other.number_of_floors
 
-    def __ne__(self, other):
+    def __ne__(self, other): #
         return self.number_of_floors != other.number_of_floors
 
-    def __add__(self, value):
+    def __add__(self, value): #
         if isinstance(value, int):
             self.number_of_floors += value
             return self
 
-    def __radd__(self, value):
+    def __radd__(self, value): #
         self.number_of_floors += value
         return self
 
-    def __iadd__(self, value):
+    def __iadd__(self, value): #
         if isinstance(value, int):
             self.number_of_floors += value
             return self
 
-    houses_history = []
+    houses_history = [] #
 
-    def __new__(cls, *args):
+    def __new__(cls, *args): #
         cls.houses_history.append(args[0])
         return object.__new__(cls)
 
-    def __init__(self, name, number_of_floors):
+    def __init__(self, name, number_of_floors): #
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def __del__(self):
+    def __del__(self): #
         print(f'{self.name} снесен, но он останется в истории')
 
 
-h1 = House('ЖК Эльбрус', 10)
-print(House.houses_history)
-h2 = House('ЖК Акация', 20)
-print(House.houses_history)
-h3 = House('ЖК Матрёшки', 20)
-print(House.houses_history)
+h1 = House('ЖК Эльбрус', 10) #
+print(House.houses_history) #
+h2 = House('ЖК Акация', 20)  #
+print(House.houses_history) #
+h3 = House('ЖК Матрёшки', 20) #
+print(House.houses_history) #
 
-# Удаление объектов
-del h2
-del h3
+del h2 # удаление объектов
+del h3 #-//-//-
 
-print(House.houses_history)
+print(House.houses_history) #
